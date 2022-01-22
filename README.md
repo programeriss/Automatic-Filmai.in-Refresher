f038ba16a75c300aef0f8573e46457e6694e0c6d
Harry Potter <programeriss@gmail.com>
2022-01-22 11:09:16 +0200

    filmai.in

---
+++ README.md
@@ -0,0 +1,13 @@
+# Automatic Filmai.in Refresher
+
+## Start project on server
+
+```
+
+
+Edit: login.py, by the comments: 
+# enter your filmai.in username here
+# enter you filmai.in password here
+
+
+```
---
+++ login.py
@@ -0,0 +1,39 @@
+from seleniumwire import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+import json
+import os
+import sys
+import pickle
+from requests import Session
+
+chromeOptions = Options()
+# chromeOptions.add_argument('--headless') # comment if you want to see the browser window visually
+chromeOptions.add_argument("--no-sandbox")
+chromeOptions.add_argument("--window-size=1280,720")
+chromeOptions.add_argument('--disable-gpu')
+chromeOptions.add_argument('--disable-dev-shm-usage')
+chromeOptions.add_argument('--disable-web-security')
+chromeOptions.add_argument('-–allow-file-access-from-files')
+chromeOptions.add_argument('--disable-site-isolation-trials')
+
+caps = DesiredCapabilities.CHROME
+caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+
+browser = webdriver.Chrome(executable_path="./chromedriver", options=chromeOptions, desired_capabilities=caps)
+
+browser.get("http://37.221.162.250/index.php?do=login")
+
+time.sleep(10)
+
+inputElement = browser.find_element_by_id("login_append")
+inputElement.send_keys('username') # enter your filmai.in username here
+
+inputElement = browser.find_element_by_id("password_append")
+inputElement.send_keys('password') # enter you filmai.in password here
+inputElement.send_keys(Keys.ENTER)
+
+time.sleep(10)
+browser.quit()
